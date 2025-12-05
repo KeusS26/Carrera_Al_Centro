0a5d16c3a8d64f2e8856b648f5f01f6c59e1a514
keuss <rodri.esteb5@gmail.com>
2025-12-05 03:17:34 -0500

    Commit de la clase Jugador y de la Clase Dado

---
+++ Jugador.h
@@ -0,0 +1,46 @@
+//
+// Created by keuss on 5/12/2025.
+//
+
+#ifndef CARRERA_AL_CENTRO_JUGADOR_H
+#define CARRERA_AL_CENTRO_JUGADOR_H
+#include <iostream>
+using namespace std;
+
+enum class Direccion {ARRIBA,ABAJO,IZQUIERDA,DERECHA};
+
+class Jugador {
+private:
+    string nombre;
+    int idJugador;
+    int pv;
+    int fila;
+    int columna;
+    bool eliminado;
+public:
+    /// Constructor
+    Jugador();
+    Jugador(string nombre, int idJugador);
+
+    /// Moetodos de Vida
+    void restarPV(int cantidad);
+    void agregarPv(int cantidad);
+    bool estaEliminado();
+
+    ///Movimiento
+    void Mover(Direccion dir,int pasos);
+
+    ///Setter
+    void setPosicion(int fila, int Columna);
+
+    ///Getter
+    string getNombre();
+    int getIdJugador();
+    int getPv();
+    int getFila();
+    int getColumna();
+    bool getEliminado();
+};
+
+
+#endif //CARRERA_AL_CENTRO_JUGADOR_H
